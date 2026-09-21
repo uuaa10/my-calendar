8899bc884d1cb98242c9dcac301c27cc67e995b8
uuaa10 <tkngyk@gmail.com>
2026-09-21 15:44:53 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='calendar-pwa-v1';
+const FILES=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
